3a93746cc4e799521b4b9ee9cef8445958cd7f72
christian.campos <christian.campos@pti.org.br>
2024-01-19 16:45:17 -0300

    Implementado endpoint product, category and metods

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -8,10 +8,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.enums.OrderStatus;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.ProductRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
@@ -22,6 +26,10 @@ public class TestConfig implements CommandLineRunner {
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -32,8 +40,20 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, usuario2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, usuario1);
 		
+		Category c1 = new Category(null, "eletronicos");
+		Category c2 = new Category(null, "portateis");
+		Category c3 = new Category(null, "esportes");
+		Category c4 = new Category(null, "higiene");
+		
+		Product p1 = new Product(null, "Iphone 14 Plus 256", "Celular ultima geracao Apple", 1490.00, c2, "imgteste");
+		Product p2 = new Product(null, "Tenis Nike Hard", "Tenis mega confortavel para correr", 250.00, c3, "imgteste");
+		Product p3 = new Product(null, "Desodorante Rexona nunca abandona","Desodorante masculino", 20.00, c4, "imgteste");
+		Product p4 = new Product(null, "Notebook Dell G3 Expiron", "Notebook Gammer Profissional", 1975.00, c1, "imgteste");
+		
 		userRepository.saveAll(Arrays.asList(usuario1, usuario2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
 		
 	}
 }
--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	/**
 	 * 
 	 */
@@ -23,16 +25,21 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
 	public Product() {
-		
+
 	}
 
-	public Product(Long id, String name, String description, Double price, String imgUrl) {
+	public Product(Long id, String name, String description, Double price, Category category, String imgUrl) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
+		this.category = category;
 		this.imgUrl = imgUrl;
 	}
 
@@ -68,6 +75,14 @@ public class Product implements Serializable{
 		this.price = price;
 	}
 
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
 	public String getImgUrl() {
 		return imgUrl;
 	}
@@ -79,7 +94,7 @@ public class Product implements Serializable{
 	@Override
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
-				+ ", imgUrl=" + imgUrl + "]";
+				+ ", imgUrl=" + imgUrl + ", category=" + category + "]";
 	}
 
 	@Override
@@ -98,9 +113,5 @@ public class Product implements Serializable{
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
+
 }
